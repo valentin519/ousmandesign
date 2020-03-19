4bf099f079a9ae69eb8d85c901fd0dbfeca454d7
valentin519 <Valentinjulien93@gmail.com>
2020-03-19 23:56:52 +0100

    mobile progress

--- ousmand/src/component/Navbar.js
+++ ousmand/src/component/Navbar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import Menu from './Menu'
 
 
 
-const Navbar = () => (
+class Navbar extends React.Component {
+	constructor(props) {
+		super(props);
+        this.state = {
+            isMenuVisible: false,
+        }
+        this.toggleMenuVisible = this.toggleMenuVisible.bind(this);
+    }
+    toggleMenuVisible() {
+		this.setState(prevState => {
+			return { isMenuVisible: !prevState.isMenuVisible };
+		});
+	}
+    render(){
+        return(
     <div>
         <div className='navbarDesktop'>
             <Link className='navbarSelection' to='/mainpage'>A propos de nous</Link>
@@ -13,7 +28,20 @@ const Navbar = () => (
             <Link className='navbarSelection' to='/contact'> Nous contacter</Link>
             <Link className='navbarSelection' to='/media'>Ils parlent de nous</Link>
         </div>
+        <div className='navbarMobile'>
+            <Link to='/' ><img className='logoNavbar' src='/ousmanlog.png' alt=''/></Link>
+            {this.state.isMenuVisible ? '' :
+            <img className='burger' src='/burgericon.png' alt=''onClick={this.toggleMenuVisible}/>
+            }
+            {this.state.isMenuVisible && (
+					<div onClick={this.toggleMenuVisible}>
+						<Menu 
+							handleChangeTab={this.handleChangeTab}/>
+					</div>
+				)}
+        </div>
     </div>
-
-)
+        )
+    }
+}
 export default Navbar
