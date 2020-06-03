03ef92a5c6d960fad99500cd144b449ac8a0ecea
valentin519 <Valentinjulien93@gmail.com>
2020-06-03 16:14:45 +0200

    corectif mep

--- ousmand/src/component/Mainpage.js
+++ ousmand/src/component/Mainpage.js
@@ -58,7 +58,7 @@ class Mainpage extends Component {
                             </div>      
                         </Slide>      
                     </div>
-                    <h2>« Ousman.O » et les Objectifs de Développement Durable (ODD-2030) de l’O.N.U.</h2>
+                    <h2>« Ousman O. » et les Objectifs de Développement Durable (ODD-2030) de l’O.N.U.</h2>
                     <div className='presentationSecond'> 
                         
                         <Slide bottom> 
@@ -78,16 +78,16 @@ class Mainpage extends Component {
                                 </p>
                             </div>      
                         </Slide> 
-                       
-                    </div>
-                    <Slide bottom> 
+                        <Slide bottom> 
                             <div className='presentationText2'>
                                 <h3>Favoriser la création et l’entreprenariat créatif et culturel.</h3>
                                 <p>
                                 La préoccupation d’<strong>Ousmane Ouedraogo</strong> est non seulement de promouvoir ses productions mais plus largement le travail de création de <strong>jeunes designers </strong>n’ayant pas accès aux mêmes opportunités (visibilité médias, ouverture aux marchés européens et nord-américains…). C’est ainsi qu’<strong>Ousmane</strong> est à l'origine du "<strong>Concours des Jeunes Designers de Mode Francophones</strong>" avec un premier défilé de clôture à l’<strong>automne 2019 </strong>aet une seconde édition en préparation pour le <strong>printemps 2021</strong>. Il s’agit bien de valoriser le travail de création et soutenir de <strong>jeunes entrepreneurs dans la mode, la culture, créateurs de valeur, créateurs de richesse</strong>. 
                                 </p>
                             </div>      
-                        </Slide>  
+                        </Slide> 
+                    </div>
+                 
                     <Slide bottom> 
                     <img className='presentationPicSecond' src='/mainpage.jpg' alt=''  /> 
                     </Slide>  
